8d704eb0ca6167322a2c213b98e1dbcbbfdd79f8
Benjamin Moore <moore.benjamin@live.com>
2021-10-20 22:46:04 -0400

    Add JDK install script

--- install7Zip.ps1
+++ install7Zip.ps1
@@ -1,1 +1,10 @@
 ﻿msiexec.exe /i 7z1900-x64.msi MSIINSTALLPERUSER=1
+
+$userPath = (Get-ItemProperty -Path HKCU:\Environment\ -Name Path).Path
+if (-not ($userPath -split ";").Contains("$env:USERPROFILE\Programs\7-Zip")) {
+    if ($userPath.EndsWith(";")) {
+        Set-ItemProperty -Path HKCU:\Environment\ -Name Path -Value "$userPath$env:USERPROFILE\Programs\7-Zip"
+    } else {
+        Set-ItemProperty -Path HKCU:\Environment\ -Name Path -Value "$userPath;$env:USERPROFILE\Programs\7-Zip"
+    }
+}
---
+++ installJDK8.ps1
@@ -0,0 +1,19 @@
+﻿mkdir jdk
+cp .\jdk-8u202-windows-x64.exe .\.jdk
+cd .\jdk
+7z.exe e .\jdk-8u202-windows-x64.exe -y
+7z.exe e .\111 -y
+mkdir jdk8
+Expand-Archive -Path .\tools.zip -DestinationPath .\jdk8
+cd .\jdk8
+$packs = Get-ChildItem -Recurse -Filter *.pack
+foreach ($pack in $packs) {
+    $newName = $pack.FullName.Substring(0, $pack.FullName.LastIndexOf(".")) + ".jar"
+    .\bin\unpack200.exe -r $pack.FullName $newName
+}
+
+cd ..
+mkdir $env:USERPROFILE\Programs -Force
+cp .\jdk8 $env:USERPROFILE\Programs -Recurse
+cd ..
+Remove-Item .\jdk -Recurse
