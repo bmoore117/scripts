de6bd035945592d07e7c29678a8a2e60da9d50a5
Benjamin Moore <moore.benjamin@live.com>
2021-10-25 18:04:24 -0400

    Add PS equivalent to linux tac, for reversing Splunk log dumps back to proper chronological order

---
+++ tac.ps1
@@ -0,0 +1,14 @@
+﻿Param(
+    [Parameter(Mandatory=$true)]
+    [String] $Path
+)
+
+$c = (Get-Content -Path $Path)
+[array]::Reverse($c)
+
+$endPath = $Path
+if ($Path.StartsWith(".\")) {
+    $endPath = $endPath.Substring(2)
+}
+
+$c | Out-File "rev-$endPath"
