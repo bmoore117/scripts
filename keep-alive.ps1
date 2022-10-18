bf84b6e67e1abaa87d99c00e74a49ebaf95d93ef
Benjamin Moore <moore.benjamin@live.com>
2022-10-17 23:30:28 -0400

    Add password reset script

---
+++ keep-alive.ps1
@@ -0,0 +1,11 @@
+﻿Clear-Host
+Write-Host "Keep-alive with Scroll Lock..."
+
+$shell = New-Object -ComObject "Wscript.Shell"
+
+while ($true) {
+    $shell.sendkeys("{SCROLLLOCK}")
+    Start-Sleep -Milliseconds 100
+    $shell.sendkeys("{SCROLLLOCK}")
+    Start-Sleep -Seconds 240
+}
