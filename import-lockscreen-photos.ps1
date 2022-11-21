b9a3641eeaf8a3f6b2b719af3f7fc8f7ab5fa33a
Benjamin Moore <american.exceptionalist@gmail.com>
2022-11-21 18:30:26 -0500

    Lockscreen photo script

---
+++ import-lockscreen-photos.ps1
@@ -0,0 +1,12 @@
+﻿if (-not (Test-Path ~\Pictures\lockscreen)) {
+    mkdir ~\Pictures\lockscreen
+}
+cp "$env:LOCALAPPDATA\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\*" ~\Pictures\lockscreen
+
+$items = Get-ChildItem ~\Pictures\lockscreen | Where-Object {$_ -isnot [IO.DirectoryInfo]}
+foreach ($item in $items) {
+    if (-not $item.FullName.EndsWith(".jpg")) {
+        mv $item.FullName ($item.FullName + ".jpg")
+    }
+}
+
