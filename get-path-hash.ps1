e35098d54a829d6575923934b62a11d4d61ae2a9
Benjamin Moore <american.exceptionalist@gmail.com>
2023-04-30 23:50:59 -0400

    add path hash script

---
+++ get-path-hash.ps1
@@ -0,0 +1,13 @@
+﻿param (
+    [Parameter(Mandatory=$true)]
+    [string] $Path
+)
+
+$item = Get-Item $Path
+
+if ($item.PSIsContainer) {
+    $hashString = (Get-ChildItem $Path -Recurse | Get-FileHash -Algorithm MD5).Hash | Out-String
+    Get-FileHash -InputStream ([IO.MemoryStream]::new([char[]]$hashString))
+} else {
+    Get-FileHash -Path $Path
+}
