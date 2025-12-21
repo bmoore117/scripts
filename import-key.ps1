e8988775348237b098ac1c9cb31c3d1aac4c14b4
Ben <moore.benjamin@live.com>
2025-12-21 09:52:08 -0500

    Finally solve mitmproxy import for all jdks

--- import-key.ps1
+++ import-key.ps1
@@ -3,4 +3,10 @@ $certificate = $certificates[0]
 $certName = $certificate.BaseName
 $path = $certificate.FullName
 
-& "$env:USERPROFILE\Programs\jdk-11.0.11\bin\keytool.exe" "importcert" "-trustcacerts" "-alias" "$certName" "-file" "$path" "-storepass" "changeit" "-noprompt" "-keystore" "$env:USERPROFILE\Programs\jdk-11.0.11\lib\security\cacerts"
+$jdks = Get-ChildItem -Filter "jdk-*" -Path "$env:USERPROFILE\Programs"
+
+foreach($jdk in $jdks) {
+	$fullName = $jdk.FullName
+	& "$fullName\bin\keytool.exe" "-importcert" "-trustcacerts" "-alias" "$certName" "-file" "$path" "-storepass" "changeit" "-noprompt" "-keystore" "$fullName\lib\security\cacerts"
+
+}
