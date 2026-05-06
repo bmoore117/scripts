44a5b9848466778ed28733be92202a92389a85a8
Ben <moore.benjamin@live.com>
2026-05-06 13:45:27 -0400

    positional parameter enhancements; file type filter capabilities

--- grep.ps1
+++ grep.ps1
@@ -1,23 +1,33 @@
 ﻿Param(
-    [Parameter(Mandatory=$false)]
-    [String] $Path,
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory=$true,Position=0)]
     [String] $Pattern,
     [Parameter(Mandatory=$false)]
-    [switch] $FilenamesOnly
+    [String] $Path,
+    [Parameter(Mandatory=$false)]
+    [String] $Filter,
+    [Parameter(Mandatory=$false)]
+    [switch] $DisplayFilenamesOnly
 )
 
 $files = $null
 
 if ($Path) {
-    $files = Get-ChildItem -Recurse -Path $Path | Where-Object {(Test-Path -Path $_.FullName -PathType Leaf) -eq $true}
+    if ($Filter) {
+        $files = Get-ChildItem -Recurse -Path $Path -Filter $Filter | Where-Object {(Test-Path -Path $_.FullName -PathType Leaf) -eq $true}
+    } else {
+        $files = Get-ChildItem -Recurse -Path $Path | Where-Object {(Test-Path -Path $_.FullName -PathType Leaf) -eq $true}
+    }
 } else {
-    $files = Get-ChildItem -Recurse -Path . | Where-Object {(Test-Path -Path $_.FullName -PathType Leaf) -eq $true}
+    if ($Filter) {
+        $files = Get-ChildItem -Recurse -Path . -Filter $Filter | Where-Object {(Test-Path -Path $_.FullName -PathType Leaf) -eq $true}
+    } else {
+        $files = Get-ChildItem -Recurse -Path . | Where-Object {(Test-Path -Path $_.FullName -PathType Leaf) -eq $true}
+    }
 }
 
 $results = $files | Select-String -Pattern ([regex]::Escape($Pattern))
 
-if ($FilenamesOnly.IsPresent) {
+if ($DisplayFilenamesOnly.IsPresent) {
     $results | Select-Object -Unique Path
 } else {
     $results
